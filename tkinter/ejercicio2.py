15eb3a88eedc932f60f54d07e2e099c23ceb83b2
Agustina Aperlo <43002670+magustinaperlo@users.noreply.github.com>
2022-09-14 13:30:09 -0300

    Update ejercicio2.py
    en la consigna: 
    2 - Un lineEdit no editable, que muestre el valor de contador y que
    inicie con el número 88

--- tkinter/ejercicio2.py
+++ tkinter/ejercicio2.py
@@ -6,8 +6,11 @@ def incrementar():
     
 ventana = tk.Tk()
 ventana.title("Contador decreciente")
+#agrego dimension de la ventana
+ventana.geometry("300x160")
 valor = tk.IntVar()
 label = tk.Label(ventana,text="Contador").pack()
+#debe inicializar en 88
 entrada = tk.Entry(ventana,textvariable=valor,state="readonly")
 entrada.pack()
 boton = tk.Button(ventana,text="-",command = incrementar).pack()
